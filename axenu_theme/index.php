733930ebbb247efb1ca44e8df1ac887073ae7295
Axenu <simonseregon@gmail.com>
2019-05-24 21:38:26 +0200

    Add background card

--- axenu_theme/index.php
+++ axenu_theme/index.php
@@ -3,52 +3,73 @@
 
 <div class="card column">
   <h3 class="header-center">Axenu does this</h3>
-  <div className="card-body">
-    <div class="item4x4">
-      <p>
-        We do this
-      </p>
-      <img src="#" />
+  <div class="card-body">
+    <div class="column column-vertical">
+      <div class="item4x4">
+        <p>
+          We do this
+        </p>
+        <img src="#" />
+      </div>
+      <div class="item4x4">
+        <p>
+          We do this
+        </p>
+        <img src="#" />
+      </div>
     </div>
-    <div class="item4x4">
-      <p>
-        We do this
-      </p>
-      <img src="#" />
-    </div>
-    <div class="item4x4">
-      <p>
-        We do this
-      </p>
-      <img src="#" />
-    </div>
-    <div class="item4x4">
-      <p>
-        We do this
-      </p>
-      <img src="#" />
+    <div class="column column-vertical">
+      <div class="item4x4">
+        <img src="#" />
+        <p>
+          We do this
+        </p>
+      </div>
+      <div class="item4x4">
+        <img src="#" />
+        <p>
+          We do this
+        </p>
+      </div>
     </div>
   </div>
 </div>
 
 <div class="card">
-  <div class="column column-vertical">
-    <h3>About Axenu</h3>
-    <img src="#" alt="Icon divider" />
-    <p>
-      vawe Duis egestas luctus tellus non sollicitudin. Morbi rutrum, risus a varius luctus, ipsum orci porta quam, ac eleifend felis orci blandit elit. Nunc blandit cursus dui, quis placerat risus elementum id. Donec ullamcorper placerat
-    </p>
-    <img src="#" alt="Icon divider" />
-    <p>
-      vawe Duis egestas luctus tellus non sollicitudin. Morbi rutrum, risus a varius luctus, ipsum orci porta quam, ac eleifend felis orci blandit elit. Nunc blandit cursus dui, quis placerat risus elementum id. Donec ullamcorper placerat
-    </p>
-    <button type="button" name="button">Read our story</button>
+  <div class="card-body">
+    <div class="column column-vertical">
+      <h3>About Axenu</h3>
+      <img src="#" alt="Icon divider" />
+      <p>
+        vawe Duis egestas luctus tellus non sollicitudin. Morbi rutrum, risus a varius luctus, ipsum orci porta quam, ac eleifend felis orci blandit elit. Nunc blandit cursus dui, quis placerat risus elementum id. Donec ullamcorper placerat
+      </p>
+      <img src="#" alt="Icon divider" />
+      <p>
+        vawe Duis egestas luctus tellus non sollicitudin. Morbi rutrum, risus a varius luctus, ipsum orci porta quam, ac eleifend felis orci blandit elit. Nunc blandit cursus dui, quis placerat risus elementum id. Donec ullamcorper placerat
+      </p>
+      <button type="button" name="button">Read our story</button>
+    </div>
+    <div class="column full-image">
+      <img src="#" alt="Image of founer"/>
+      <p class="image-text">
+        Simon Nilsson, CEO and founder of Axenu
+      </p>
+    </div>
   </div>
-  <div class="column full-image">
-    <img src="#" alt="Image of founer"/>
-    <p class="image-text">
-      Simon Nilsson, CEO and founder of Axenu
-    </p>
+</div>
+
+<div class="card">
+  <div class="card-body">
+
+    <div class="column full-image">
+      <img src="#" alt="Image of founer"/>
+    </div>
+    <div class="column column-vertical">
+      <h3>Background</h3>
+      <p>
+        vawe Duis egestas luctus tellus non sollicitudin. Morbi rutrum, risus a varius luctus, ipsum orci porta quam, ac eleifend felis orci blandit elit. Nunc blandit cursus dui, quis placerat risus elementum id. Donec ullamcorper placerat
+      </p>
+    </div>
   </div>
 </div>
 
