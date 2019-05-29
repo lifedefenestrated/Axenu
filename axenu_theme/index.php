3ab7418d4c5f9f051540a86f5ecc9f327dce505f
Axenu <simonseregon@gmail.com>
2019-05-29 21:50:28 +0200

    Add design for plugin blocks

--- axenu_theme/index.php
+++ axenu_theme/index.php
@@ -1,35 +1,65 @@
 <?php get_header(); ?>
 
 
-<div class="card column">
-  <h3 class="header-center">Axenu does this</h3>
+<div class="card">
+  <h3>Header</h3>
   <div class="card-body">
-    <div class="column column-vertical">
-      <div class="item4x4">
-        <p>
-          We do this
-        </p>
-        <img src="#" />
+    <div class="card-column">
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo…
+      </p>
+    </div>
+    <div class="card-column">
+      <p>
+        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliquam quaerat voluptatem. Ut enim ad minima veniam, quis nostrum exercitationem ullam corporis suscipit laboriosam, nisi ut aliquid ex ea commodi consequatur? Quis autem vel eum iure reprehenderit qui in ea voluptate velit esse quam nihil molestiae consequatur, vel illum qui dolorem eum fugiat quo voluptas nulla pariatur? Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo…
+      </p>
+    </div>
+  </div>
+</div>
+
+<div class="card">
+  <h3 class="center">Axenu does this</h3>
+  <div class="card-body">
+    <div class="card-column">
+      <div>
+        <div class="flex-column flex-6">
+          <h4>We do this</h4>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore v
+          </p>
+        </div>
+        <div class="flex-4">
+          <img class="round" src="<?php echo get_bloginfo('template_directory'); ?>/images/frame-2.png" />
+        </div>
       </div>
-      <div class="item4x4">
-        <p>
-          We do this
-        </p>
-        <img src="#" />
+      <div>
+        <div class="flex-column flex-6">
+          <h4>We do this</h4>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore v
+          </p>
+        </div>
+        <img class="round flex-4" src="#" />
       </div>
     </div>
-    <div class="column column-vertical">
-      <div class="item4x4">
-        <img src="#" />
-        <p>
-          We do this
-        </p>
+    <div class="card-column">
+      <div>
+        <img class="round flex-4" src="#" />
+        <div class="flex-column flex-6">
+          <h4>We do this</h4>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore v
+          </p>
+        </div>
       </div>
-      <div class="item4x4">
-        <img src="#" />
-        <p>
-          We do this
-        </p>
+      <div>
+        <img class="round flex-4" src="#" />
+        <div class="flex-column flex-6">
+          <h4>We do this</h4>
+          <p>
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore v
+          </p>
+        </div>
       </div>
     </div>
   </div>
@@ -37,20 +67,20 @@
 
 <div class="card">
   <div class="card-body">
-    <div class="column column-vertical">
+    <div class="card-column">
       <h3>About Axenu</h3>
-      <img src="#" alt="Icon divider" />
+      <img src="#" alt="Icon divider" class="divider" />
       <p>
         vawe Duis egestas luctus tellus non sollicitudin. Morbi rutrum, risus a varius luctus, ipsum orci porta quam, ac eleifend felis orci blandit elit. Nunc blandit cursus dui, quis placerat risus elementum id. Donec ullamcorper placerat
       </p>
-      <img src="#" alt="Icon divider" />
+      <img src="#" alt="Icon divider" class="divider"/>
       <p>
         vawe Duis egestas luctus tellus non sollicitudin. Morbi rutrum, risus a varius luctus, ipsum orci porta quam, ac eleifend felis orci blandit elit. Nunc blandit cursus dui, quis placerat risus elementum id. Donec ullamcorper placerat
       </p>
       <button type="button" name="button">Read our story</button>
     </div>
-    <div class="column full-image">
-      <img src="#" alt="Image of founer"/>
+    <div class="card-column">
+      <img src="#" alt="Image of founer" class="round"/>
       <p class="image-text">
         Simon Nilsson, CEO and founder of Axenu
       </p>
@@ -61,14 +91,81 @@
 <div class="card">
   <div class="card-body">
 
-    <div class="column full-image">
+    <div class="card-column">
       <img src="#" alt="Image of founer"/>
     </div>
-    <div class="column column-vertical">
+    <div class="card-column">
       <h3>Background</h3>
       <p>
         vawe Duis egestas luctus tellus non sollicitudin. Morbi rutrum, risus a varius luctus, ipsum orci porta quam, ac eleifend felis orci blandit elit. Nunc blandit cursus dui, quis placerat risus elementum id. Donec ullamcorper placerat
       </p>
+    </div>
+  </div>
+</div>
+
+<div class="card">
+  <h3 class="center">Work with us</h3>
+  <div class="card-body">
+    <div class="card-row">
+      <div class="card-column">
+        <img src="#" alt="Image of founer" class="round divider"/>
+        <h4>Background</h4>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore v
+        </p>
+      </div>
+      <div class="card-column">
+        <img src="#" alt="Image of founer" class="round divider"/>
+        <h4>Background</h4>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore v
+        </p>
+      </div>
+      <div class="card-column">
+        <img src="#" alt="Image of founer" class="round divider"/>
+        <h4>Background</h4>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore v
+        </p>
+      </div>
+    </div>
+  </div>
+</div>
+
+<div class="card">
+  <h3 class="center">Latest blog posts</h3>
+  <div class="card-body">
+    <div class="card-row">
+      <div class="card-column">
+        <img src="#" alt="Image of founer" />
+        <h4>Background</h4>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore v
+        </p>
+        <p>
+          2 Nov 2018
+        </p>
+      </div>
+      <div class="card-column">
+        <img src="#" alt="Image of founer"/>
+        <h4>Background</h4>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore v
+        </p>
+        <p>
+          2 Nov 2018
+        </p>
+      </div>
+      <div class="card-column">
+        <img src="#" alt="Image of founer"/>
+        <h4>Background</h4>
+        <p>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore v
+        </p>
+        <p>
+          2 Nov 2018
+        </p>
+      </div>
     </div>
   </div>
 </div>
