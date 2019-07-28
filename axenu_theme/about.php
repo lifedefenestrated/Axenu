0c072721972c259449ba22eac93871ad755d499e
Axenu <simonseregon@gmail.com>
2019-07-28 22:14:13 +0200

    Remove old files

--- axenu_theme/about.php
+++
@@ -1,7 +0,0 @@
-<?php /* Template Name: About */ get_header(); ?>
-<div id="primary">
-  <div id="content" role="main">
-    <h1>About</h1>
-  </div><!-- #content -->
-</div><!-- #primary -->
-<?php get_footer(); ?>
